05d289d508a731c7ea9dac33d6ecf674a598b4b2
Marty Zhou <martyzhou@Objectivasoftware.com>
2016-10-27 21:40:48 +0800

    more test

---
+++ netcore/Xml/XmlSpec.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using Xunit;
+
+namespace netcore.Xml
+{
+    public class XmlSpec
+    {
+        [Fact]
+        public void XmlParse()
+        {
+            XmlDocument xml = new XmlDocument();
+            FileStream fileStream = new FileStream("./Xml/bbcseed.xml", FileMode.Open);
+            xml.Load(fileStream);
+
+            XmlNodeList feeds = xml["rss"]["channel"].ChildNodes;
+
+            //foreach(XmlNode node in feeds)
+            //{
+            //    if(node.Name == "item")
+            //    {
+            //        Console.WriteLine(node["link"].InnerText);
+            //    }
+            //}
+
+            // Assert.Equal("rss", xml.ChildNodes[2].Name);
+        }
+
+        [Fact]
+        public void HtmlParse()
+        {
+            // For security reasons DTD is prohibited in this XML document. To enable DTD processing set the DtdProcessing property on XmlReaderSettings to Parse and pass the settings into XmlReader.Create method.
+            XmlDocument xml = new XmlDocument();
+            XmlReaderSettings xmlReaderSettings = new XmlReaderSettings();
+            xmlReaderSettings.DtdProcessing = DtdProcessing.Ignore;
+            xmlReaderSettings.IgnoreComments = true;
+            xmlReaderSettings.IgnoreProcessingInstructions = true;
+            xmlReaderSettings.IgnoreWhitespace = true;
+            XmlReader xmlReader = XmlReader.Create(new FileStream("./Http/bbcnews.html", FileMode.Open), xmlReaderSettings);
+
+            xml.Load(xmlReader);
+
+            //string html = File.ReadAllText("./Http/bbcnews.html");
+            //xml.LoadXml(html);
+        }
+    }
+}
