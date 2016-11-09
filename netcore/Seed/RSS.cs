9cc1ae8caee9d8bfb3ecb90f2dbdd21b141fd1f4
Marty Zhou <martyzhou@Objectivasoftware.com>
2016-11-09 15:04:28 +0800

    test

---
+++ netcore/Seed/RSS.cs
@@ -0,0 +1,6 @@
+
+namespace Seed{
+    public class RSS{
+        
+    }
+}
---
+++ netcore/Seed/RSSItem.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Seed
+{
+    public class RSSItem
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+        public DateTime PubDate { get; set; }
+    }
+
+}
