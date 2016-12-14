862fed1b39297dbf4a48326f5def6aa98a8b3c8e
Marty Zhou <martyzhou@Objectivasoftware.com>
2016-12-14 15:08:54 +0800

    exif

--- js/geotravel/save_pic_metadata.js
+++ js/geotravel/save_pic_metadata.js
@@ -1,35 +1,49 @@
-document.getElementById("objecturltest").onclick = function () {
-        var http = new XMLHttpRequest();
-        
-        http.open("GET", photos[clicktimes].url, true);
-        http.responseType = "blob";
-        http.onload = function (e) {
-            if (this.status === 200) {
-                var image = new Image();
-                image.onload = function () {
-                    EXIF.getData(image, function () {
-                        var exifdata = this.exifdata;
-                        var lat = exifdata.GPSLatitude;
-                        var lon = exifdata.GPSLongitude;
-                        var latRef = exifdata.GPSLatitudeRef;
-                        var lonRef = exifdata.GPSLongitudeRef;
-                        if (lat && lon && latRef && lonRef) {
-                            var latResult = (lat[0] + lat[1] / 60 + lat[2] / 3600) * (latRef == "N" ? 1 : -1);
-                            var lonResult = (lon[0] + lon[1] / 60 + lon[2] / 3600) * (lonRef == "W" ? -1 : 1);
+var http = require('http');
+var fs = require('fs');
+var querystring = require('querystring');
+var exif = require("./node_modules/exif-js/exif.js");
+    
+exports.save_pic_metadata = function(req, res, next) {
+    /*console.log("headers " + req.method);
+    console.log(req.headers);
+    console.log("rawHeaders");
+    console.log(req.rawHeaders);
+    console.log("rawTrailers");
+    console.log(req.rawTrailers);*/
 
-                            var location = { lat: latResult, lng: lonResult };
+    if(req.method === "POST"){
+        var body='';
 
-                            if (gmap) {
-                                var testMarker = new google.maps.Marker({
-                                    position: location,
-                                    map: gmap
-                                })
-                            }
-                        }
-                    });
-                };
-                image.src = URL.createObjectURL(http.response);
-            }
-        };
-        http.send();
+        req.on('data', (chunk) => {
+            body += chunk;
+            console.log(`BODY: ${chunk}`);
+        });
+
+        req.on('end', () => {
+            var data = querystring.parse(body);
+            var pic_url = data.pic_url;
+            
+            extractExif(pic_url, (imgData) => {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write('<html><body><img src="data:image/jpeg;base64,')
+                res.write(new Buffer(imgData).toString('base64'));
+                res.end('"/></body></html>');
+            });
+        });
+    }else{
+        res.end("method not supported.");
     }
+}
+
+function extractExif(url, callback){
+    /*http.get(url, (res) => {
+
+        callback();
+    });*/
+    fs.readFile('./imgs/IMG_5136.JPG', (err, data) => {
+        EXIF.getData(data, () => {
+            
+        });
+        callback(data);
+    });
+}
