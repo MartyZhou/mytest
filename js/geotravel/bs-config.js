0fdf4c729da318d274e1f148564dd39f0ef059b5
Marty Zhou <martyzhou@Objectivasoftware.com>
2016-12-15 12:26:00 +0800

    use google drive api

--- js/geotravel/bs-config.js
+++ js/geotravel/bs-config.js
@@ -11,7 +11,7 @@ module.exports = {
     "watchOptions": {},
     "server": true,
     "proxy": false,
-    "port": 3000,
+    "port": 3005,
     "middleware": [
         {
             route: "/save_pic_metadata",
