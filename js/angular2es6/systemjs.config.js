30e7f8b8068930c1d8d7754bef0f006f0b3f5999
Marty Zhou <martyzhou@Objectivasoftware.com>
2017-03-10 11:53:37 +0800

    use babel

--- js/angular2es6/systemjs.config.js
+++ js/angular2es6/systemjs.config.js
@@ -1,7 +1,6 @@
 System.config({
     baseURL: '/',
-    defaultJSExtensions: true,
-    transpiler: 'babel',
+    transpiler: 'plugin-babel',
     babelOptions: {
         optional: [
             'runtime',
@@ -10,9 +9,11 @@ System.config({
         ]
     },
     paths: {
-        'npm:*': 'node_modules/*'
+        'npm:': 'node_modules/'
     },
     map: {
-        'babel': 'npm:babel-core'
+        'plugin-babel': 'npm:systemjs-plugin-babel/plugin-babel.js',
+        'systemjs-babel-build': 'npm:systemjs-plugin-babel/systemjs-babel-node.js',
+        '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/index.js'
     }
 })
---
+++ js/babelstudy/main.js
@@ -0,0 +1,7 @@
+import 'reflect-metadata'; // es7.decorators requires reflect-metadata
+import 'zone.js' // Angular requires Zone.js prolyfill.
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
