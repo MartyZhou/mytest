7fd30c03568ea6e0d427302c4d0cbe853f0515ff
Marty Zhou <martyzhou@outlook.com>
2017-03-11 00:51:20 +0800

    test

--- js/babelstudy/app/app.module.js
+++ js/babelstudy/app/app.module.js
@@ -2,12 +2,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
-    imports: [ BrowserModule, FormsModule ],
-    declarations: [ AppComponent, HeroDetailComponent ],
-    bootstrap: [ AppComponent ]
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    declarations: [
+        AppComponent,
+        HeroDetailComponent,
+        HeroesComponent
+    ],
+    providers: [
+        HeroService
+    ],
+    bootstrap: [AppComponent]
 })
 
 export class AppModule { }
