f07ad7a251c8dd7f5a3fa8b6109abfe442c6073f
Marty Zhou <martyzhou@Objectivasoftware.com>
2017-03-20 16:28:11 +0800

    use card

--- js/ionictutorial/src/app/in-memory-data.service.ts
+++ js/ionictutorial/src/app/in-memory-data.service.ts
@@ -2,16 +2,16 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     let heroes = [
-      {id: 11, name: 'Mr. Nice'},
-      {id: 12, name: 'Narco'},
-      {id: 13, name: 'Bombasto'},
-      {id: 14, name: 'Celeritas'},
-      {id: 15, name: 'Magneta'},
-      {id: 16, name: 'RubberMan'},
-      {id: 17, name: 'Dynama'},
-      {id: 18, name: 'Dr IQ'},
-      {id: 19, name: 'Magma'},
-      {id: 20, name: 'Tornado'}
+      {id: 11, name: 'Mr. Nice', power: 'Really Smart'},
+      {id: 12, name: 'Narco', power: 'Super Flexible'},
+      {id: 13, name: 'Bombasto', power: 'Super Hot'},
+      {id: 14, name: 'Celeritas', power: 'Weather Changer'},
+      {id: 15, name: 'Magneta', power: 'Super Hot'},
+      {id: 16, name: 'RubberMan', power: 'Super Flexible'},
+      {id: 17, name: 'Dynama', power: 'Really Smart'},
+      {id: 18, name: 'Dr IQ', power: 'Super Flexible'},
+      {id: 19, name: 'Magma', power: 'Weather Changer'},
+      {id: 20, name: 'Tornado', power: 'Super Hot'}
     ];
     return {heroes};
   }
