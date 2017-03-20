767720bf7318847aeba5275f0b94f2b5038ff1ee
Marty Zhou <martyzhou@Objectivasoftware.com>
2017-03-20 16:57:32 +0800

    svg

--- js/ng2ts/src/app/dashboard.component.js
+++ js/ng2ts/src/app/dashboard.component.js
@@ -14,11 +14,26 @@ var DashboardComponent = (function () {
     function DashboardComponent(heroService) {
         this.heroService = heroService;
         this.heroes = [];
+        this.items = [];
+        this.items = [
+            { x: 51.6, y: 162.27272727272725, w: 64.8, h: 24.545454545454547 },
+            { x: 159.6, y: 186.8181818181818, w: 64.8, h: 73.63636363636364 },
+            { x: 267.6, y: 64.09090909090908, w: 64.8, h: 122.72727272727273 },
+            { x: 375.6, y: 15, w: 64.8, h: 171.8181818181818 },
+            { x: 483.6, y: 186.8181818181818, w: 64.8, h: 98.18181818181819 }
+        ];
     }
     DashboardComponent.prototype.ngOnInit = function () {
         var _this = this;
         this.heroService.getHeroes()
             .then(function (heroes) { return _this.heroes = heroes.slice(1, 5); });
+        this.items = [
+            { x: 51.6, y: 162.27272727272725, w: 64.8, h: 24.545454545454547 },
+            { x: 159.6, y: 186.8181818181818, w: 64.8, h: 73.63636363636364 },
+            { x: 267.6, y: 64.09090909090908, w: 64.8, h: 122.72727272727273 },
+            { x: 375.6, y: 15, w: 64.8, h: 171.8181818181818 },
+            { x: 483.6, y: 186.8181818181818, w: 64.8, h: 98.18181818181819 }
+        ];
     };
     DashboardComponent = __decorate([
         core_1.Component({
--- js/ng2ts/src/app/dashboard.component.ts
+++ js/ng2ts/src/app/dashboard.component.ts
@@ -11,11 +11,28 @@ import { HeroService } from './hero.service';
 })
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
-
-  constructor(private heroService: HeroService) { }
+  items: any[] = [];
+  
+  constructor(private heroService: HeroService) { 
+    this.items = [
+{x: 51.6, y: 162.27272727272725, w: 64.8, h: 24.545454545454547},
+{x: 159.6, y: 186.8181818181818, w: 64.8, h: 73.63636363636364},
+{x: 267.6, y: 64.09090909090908, w: 64.8, h: 122.72727272727273},
+{x: 375.6, y: 15, w: 64.8, h: 171.8181818181818},
+{x: 483.6, y: 186.8181818181818, w: 64.8, h: 98.18181818181819}
+    ];
+  }
 
   ngOnInit(): void {
     this.heroService.getHeroes()
       .then(heroes => this.heroes = heroes.slice(1, 5));
+
+    this.items = [
+{x: 51.6, y: 162.27272727272725, w: 64.8, h: 24.545454545454547},
+{x: 159.6, y: 186.8181818181818, w: 64.8, h: 73.63636363636364},
+{x: 267.6, y: 64.09090909090908, w: 64.8, h: 122.72727272727273},
+{x: 375.6, y: 15, w: 64.8, h: 171.8181818181818},
+{x: 483.6, y: 186.8181818181818, w: 64.8, h: 98.18181818181819}
+    ];
   }
 }
